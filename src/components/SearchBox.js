e744bbd27b58387d5ebfe34728c87f459a48113d
root <segunorodeji@gmail.com>
2020-03-09 21:47:04 +0100

    done

---
+++ src/components/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox = ({searchField, searchChange}) => {
+    return(
+        <div className= 'pa3 '>
+            <input className='pa3 ba b--green bg-lightest-blue' 
+                type='search' placeholder='search robots' onChange={searchChange}></input>
+        </div>        
+    )    
+}
+
+export default SearchBox;
